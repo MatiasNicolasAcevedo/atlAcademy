0f27e1cd426613f3d5c96171632844b4db2b34fa
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2023-06-10 12:21:33 -0300

    Ejercicios Clase 6

--- Clase6/Ejercicio8PiedraPapelTijera.java
+++ Clase6/Ejercicio8PiedraPapelTijera.java
@@ -4,11 +4,25 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Ejercicio8PiedraPapelTijera {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Scanner teclado = new Scanner(System.in);
-            Random aleatorio = new Random();
-            int opcionPC = aleatorio.nextInt(3);
+        Scanner teclado = new Scanner(System.in);
+        Random aleatorio = new Random();
+        int opcionUsuario;
+        int opcionPC;
+
+        System.out.println("[Bienvenido al Juego: Piedra, Papel ó Tijera]");
+        System.out.println("Elija un número para hacer la jugada: [ 1) Piedra ] [ 2) Papel ] [ 3) Tijera ]");
+
+        opcionUsuario = teclado.nextInt();
+        opcionPC = aleatorio.nextInt(3)+1; // devuelve un random desde 0 hasta 3. (0,1,2). Le sumamos 1 para que coincida con el menu.
+
+        if (opcionUsuario == opcionPC){
+            System.out.println("Ha empatado:");
+            System.out.println("Jugador: " + opcionUsuario);
+            System.out.println("Computadora: " + opcionPC);
+        }
+
 
 
 
