8e9e3e38ef40174127747820eadfed12f241462c
Matias Nicolas Acevedo <matias.nicolas.acevedo@gmail.com>
2023-06-14 15:55:32 -0300

    Actualizando

--- Clase6/Ejercicio4EligeTuAventura.java
+++ Clase6/Ejercicio4EligeTuAventura.java
@@ -1,40 +1,58 @@
 package Clase6;
 
+import java.util.Scanner;
+
 public class Ejercicio4EligeTuAventura {
     public static void main(String[] args) {
 
-        /*
-        Nombre del futbolista: Diego Rodríguez
+        Scanner scanner = new Scanner(System.in);
 
-        Historia:
-        Diego Rodríguez es un joven talentoso que sueña con convertirse en un futbolista profesional. Desde muy pequeño, mostró habilidades innatas en el deporte y siempre fue elogiado por su dedicación y trabajo duro. Ahora, a los 18 años, tiene la oportunidad de unirse a un prestigioso club de fútbol y dar el siguiente paso hacia su sueño. Pero, debe tomar una decisión crucial:
+        System.out.println("¡Bienvenido a la historia del futbolista!");
+        System.out.println("Eres un talentoso jugador de fútbol y debes tomar decisiones clave en tu carrera.");
+        System.out.println("Estás en la final de la Copa del Mundo. Tienes el balón en tus pies, y dos jugadores rivales se acercan a toda velocidad ¿Qué quieres hacer?");
+        System.out.println("1. Intentar anotar el gol decisivo.");
+        System.out.println("2. Pasar el balón a un compañero de equipo.");
+        int decision1 = scanner.nextInt();
 
-        Camino 1: Seguir la carrera en el club local
+        if (decision1 == 1) {
+            System.out.println("¡Decidiste intentar anotar el gol decisivo!");
+            System.out.println("¿Qué camino quieres tomar ahora?");
+            System.out.println("1. Intentar gambetear a los rivales y tomar un disparo desde fuera del área.");
+            System.out.println("2. Realizar un regate, abrirse por la banda y luego centrar el balón.");
+            int decision2 = scanner.nextInt();
 
-        Diego decide quedarse en su club local y seguir desarrollándose como futbolista:
+            if (decision2 == 1) {
+                System.out.println("Logras evadir con éxito a los rivales..");
+                System.out.println("¡Tomaste un disparo desde fuera del área!");
+                System.out.println("¡GOOOOL! ¡Has anotado el gol de tu vida y tu equipo es campeón del mundo!");
+                System.out.println("¡Felicidades, eres una leyenda del fútbol!");
+            } else if (decision2 == 2) {
+                System.out.println("¡Realizaste un regate y luego centraste el balón!");
+                System.out.println("Lamentablemente, el portero atrapó tu disparo.");
+                System.out.println("Salieron de contra y el equipo contrario anota gol");
+                System.out.println("Tu equipo perdió la final de la Copa del Mundo.");
+                System.out.println("No te preocupes, aún tienes tiempo para redimirte.");
+            }
+        } else if (decision1 == 2) {
+            System.out.println("¡Decidiste pasar el balón a un compañero de equipo!");
+            System.out.println("¿Qué camino quieres tomar ahora?");
+            System.out.println("1. Pasar el balón a un compañero en la delantera.");
+            System.out.println("2. Pasar el balón a un compañero en el medio campo.");
+            int decision3 = scanner.nextInt();
 
-        1.1 Entrenamiento constante:
-        Diego se compromete a entrenar más duro que nunca. Asiste a cada sesión de entrenamiento, trabaja en su técnica y mejora sus habilidades. Su esfuerzo y dedicación lo llevan a ser el mejor jugador de su equipo.
-
-        Desenlace 1.1.1: Éxito en el club local:
-        Gracias a su excelente desempeño en el club local, Diego es convocado para jugar en la selección juvenil de su país. Se convierte en una promesa del fútbol y atrae la atención de varios clubes profesionales. Al final, logra fichar por un equipo de la primera división y cumple su sueño de convertirse en futbolista profesional.
-
-        Desenlace 1.1.2: Lesión y superación:
-        Desafortunadamente, durante un partido importante, Diego sufre una grave lesión en la rodilla. A pesar de los pronósticos desalentadores, él se niega a rendirse y se somete a una intensa rehabilitación. Después de meses de arduo trabajo, logra recuperarse por completo y vuelve a jugar con más fuerza y determinación que nunca. Su increíble regreso le gana el reconocimiento de los aficionados y los medios, y finalmente es fichado por un club profesional.
-
-                Camino 2: Unirse a un club extranjero
-
-        Diego decide aprovechar la oportunidad de unirse a un club extranjero:
-
-        2.1 Adaptación en el extranjero:
-        Diego se enfrenta a nuevos desafíos al mudarse a un país extranjero. Tiene que adaptarse a un nuevo idioma, cultura y estilo de juego. Sin embargo, su talento y dedicación lo ayudan a superar los obstáculos iniciales y a destacarse en su nuevo equipo.
-
-                Desenlace 2.1.1: Éxito en el extranjero:
-        Diego se convierte rápidamente en una pieza clave de su equipo extranjero. Su rendimiento excepcional le valen el reconocimiento internacional y pronto es fichado por un club de élite. Logra ganar varios títulos y se convierte en uno de los futbolistas más reconocidos y admirados de su generación.
-
-                Desenlace 2.1.2: Dificultades y perseverancia:
-        Aunque al principio las cosas no salen como Diego esperaba, él no se rinde. Trabaja incansablemente para superar las barreras del idioma y las diferencias culturales. A medida que pasa el tiempo, se gana el respeto de sus compañeros y entrenadores con su talento y ética de trabajo. Finalmente, logra brillar en el equipo y, con el tiempo, su éxito lo lleva a representar a su selección nacional y a cumplir su sueño de ser un futbolista profesional reconocido.
-        */
-
+            if (decision3 == 1) {
+                System.out.println("¡Pasaste el balón a un compañero en la delantera!");
+                System.out.println("Tu compañero anotó el gol y tu equipo ganó la final de la Copa del Mundo.");
+                System.out.println("Eres un jugador generoso y tu visión de juego es admirada por todos.");
+                System.out.println("¡Felicidades, has dejado tu marca en la historia del fútbol!");
+            } else if (decision3 == 2) {
+                System.out.println("¡Pasaste el balón a un compañero en el medio campo!");
+                System.out.println("Lamentablemente, tu compañero perdió el balón y el equipo contrario anotó.");
+                System.out.println("Tu equipo perdió la final de la Copa del Mundo.");
+                System.out.println("No te preocupes, aún tienes tiempo para redimirte.");
+            }
+        } else {
+            System.out.println("¡Opción inválida! La historia ha terminado.");
+        }
     }
 }
